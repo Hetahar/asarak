53f0c37a8d52da97098e1b74126cc239e4bc6006
Heta <hetahartzell@protonmail.com>
2025-03-01 22:47:09 +0200

    final working version

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 import Footer from '../components/Footer';
 import FormHero from '../components/FormHero';
 import ContactInfo from '../components/ContactInfo';
